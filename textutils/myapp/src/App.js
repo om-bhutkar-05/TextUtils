63ecb736a9caf04d1314c064a6c0aadd82bbd81d
om <ombhutkar9@gmail.com>
2024-06-18 22:06:20 +0530

    added dark mode

--- textutils/myapp/src/App.js
+++ textutils/myapp/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import './textutils.css';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar.js'
 import Tbox from './components/Textform.js'
+import About from './components/about.js'
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
   return (
 <>
-<Navbar title="TextUtils"/>
+
+<div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+ <Navbar title="TextUtils" isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
 <div className='container'>
 <Tbox  heading="Enter your Text to be analyzed" />
+<About isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/> 
+</div>
 </div>
 </>
   );
--- textutils/myapp/src/components/Textform.js
+++ textutils/myapp/src/components/Textform.js
@@ -1,22 +1,54 @@
 import React,{useState} from 'react'
 
 export default function Textform(props) {
+  const [text,seText]=useState("Enter the text ");
     const upperclick=()=>{
         let newtext=text.toUpperCase();
         seText(newtext);
-    }
+      }
+    const lowerclick=()=>{
+        let newtext=text.toLowerCase();
+        seText(newtext);
+      }
+    const reset=()=>{
+        
+        seText("Enter the text ");
+      }
+    const Tbox=()=>{
+        if(text==="Enter the text "){
+        seText("");
+      }
+      }
+      const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+      }
     const changed=(event)=>{
       
       seText(event.target.value)
     }
-    const [text,seText]=useState("Enter the text ");
+    
   return (
     <>
+    <div className='container'>
 <div className="mb-3">
   <label htmlFor="Tbox" className="form-label"><h1>{props.heading}</h1></label>
-  <textarea className="form-control" id="Tbox" value={text} onChange={changed} rows="8"></textarea>
+  <textarea className="form-control" id="Tbox" value={text} onClick={Tbox} onChange={changed} rows="8"></textarea>
 </div>
-<button className='btn btn-primary' onClick={upperclick}>Convert to uppercase</button>
+<button className='btn btn-primary mx-2' onClick={upperclick}>Convert to UpperCase</button>
+<button className='btn btn-primary' onClick={lowerclick}>Convert to LowerCase</button>
+<button className='btn btn-danger mx-2' onClick={reset}>Reset</button>
+<button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+</div>
+<div className="container">
+  <h2>Your text summary : </h2>
+  <p>number of words : {text.split(/\s+/).length-1}</p>
+  <p>number of characters: {text.length}</p>
+  <p>Average Reading time: {((text.split(/\s+/).length-1)/200)*60+" seconds"}</p>
+  <h2>Preview</h2>
+  <p>{text}</p>
+</div>
     </>
   )
 }
